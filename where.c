bab5fa16d8148c4d3ada945ab54bd0cd8389ac62
Peter Haushalter <peterhaushalter@peters-mbp.myfiosgateway.com>
2020-10-06 20:23:14 -0400

    added where.c

---
+++ where.c
@@ -0,0 +1,40 @@
+#include "get_path.h"
+#include <string.h>
+
+char **where(char *command, struct pathelement *path){
+    char aPath[16][64];
+    char **pathsFound;
+    char *Found[16];
+    int commandsFound = 0;
+
+    while (path) {
+        sprintf(aPath[commandsFound], "%s/%s", path->element, command);
+
+        if (access(aPath[commandsFound], X_OK) == 0) {
+            Found[commandsFound] = calloc(strlen(aPath[commandsFound]) + 1, sizeof(char));
+            commandsFound++;
+        }else{
+            for(int index=0; index<64; index++){
+                aPath[commandsFound][index] = '0';
+            }
+        }
+
+        path = path->next;
+    }
+
+    for(int index = 0; index<commandsFound; index++){
+        printf("%s\n", aPath[index]);
+    }
+
+    if (commandsFound) {
+        pathsFound = calloc(commandsFound + 1, sizeof(char *));
+
+        for(int index = 0; index<commandsFound; index++){
+            pathsFound[index] = Found[index];
+        }
+
+        return pathsFound;
+    }else{
+        return (char **) NULL;
+    }
+}
