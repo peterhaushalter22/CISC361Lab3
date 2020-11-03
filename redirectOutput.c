b9b832188781e9193a8f1801f336484a3e18cfb0
Luke Fanizzi <lukefanizzi@Lukes-MBP-2.verizon.net>
2020-11-02 23:04:36 -0500

    Start

---
+++ redirectOutput.c
@@ -0,0 +1,17 @@
+#include <unistd.h>  //included files and libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <glob.h>
+#include <signal.h>
+#include "sh.h"
+
+
+
+
+
+void redirectOutput( char command, char file){
+	 command > file;
+
+}
